db45ae9951608cd4e0154e2441b3c25fe9e34ff2
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2024-11-11 11:51:25 -0600

    accordian complete project

---
+++ src/components/accordian/data.js
@@ -0,0 +1,33 @@
+const data =[
+  {
+    id:'1',
+    question:"what are accorion components?",
+    answer:
+    "Accordion components are user interface elements used for organ..."
+  },
+  {
+    id:'2',
+    question:"What are they used for?",
+    answer:
+    "They are commonly employed in various contexts,includidign..."
+  },
+  {
+    id:'3',
+    question:'Accordion as a musical instrument',
+    answer:
+    "Yes of course, it is very possible to create an accordion "
+  },
+  {
+    id:"4",
+    question:'  Can i create an acoordion component with different fr..',
+    answer:"Yes of course, it is very possible to create an accordion comp.."
+  }
+]
+
+
+
+
+
+
+
+export default data
